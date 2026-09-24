4f5ec9e86f3d822130795ba4f85abed995cbd19b
Bhawani <bhawanilalbxr@gmail.com>
2026-09-24 18:19:10 +0100

    Add dataset download script

---
+++ download_data.py
@@ -0,0 +1,7 @@
+import kagglehub
+
+# Download the casting product defect dataset
+
+path = kagglehub.dataset_download("ravirajsinh45/real-life-industrial-dataset-of-casting-product")
+
+print("Dataset downloaded to:", path)
